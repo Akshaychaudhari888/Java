5f14f317fc436ab652b6d566bdca7198cbae7812
Akshaychaudhari <akshaychaudhari9356@gmail.com>
2024-01-16 10:40:28 +0530

    Add new file

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Deom 
+{
+    public static void main(String arg[])
+    {
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(i+"\t");            
+        }
+    }    
+}
